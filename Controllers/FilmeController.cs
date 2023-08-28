b95bd7ec89159b21253109caab7a20fc1fa42b31
wilker.santos <wilker.santos@brktecnologia.com.br>
2023-08-28 18:53:34 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,44 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpGet]
+        public IEnumerable<Filme> ConsultaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
+        {
+            return filmes.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ConsultaFilmePorId(int id)
+        {
+            var filme = filmes.FirstOrDefault(filme => filme.Id == id);
+
+            if (filme == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(filme);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+
+            return CreatedAtAction(nameof(ConsultaFilmePorId), new { id = filme.Id }, filme);
+        }
+
+
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Titulo do filme é obrigatorio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O Genero do filme é obrigatorio")]
+        [MaxLength(50, ErrorMessage = "O tamanho do genêro não pode exceder 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "A Duracao do filme é obrigatorio")]
+        [Range(1, 360, ErrorMessage = "A duração do filme deve ter entre 1 minuto e 360")]
+        public int Duracao { get; set; }
+    }
+}
