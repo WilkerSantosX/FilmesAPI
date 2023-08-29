3e870c71f5722035e43bad83c7946d24110a67d8
Wilker Dos Santos <dev.wilkersantos@outlook.com>
2023-08-29 15:24:21 -0300

    Comentários nos métodos

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -8,6 +8,9 @@ using System.Data.SqlTypes;
 
 namespace FilmesAPI.Controllers
 {
+    ///<summary>
+    ///API que disponibiliza métodos RestFufll para Filmes
+    ///</summary>
     [ApiController]
     [Route("[controller]")]
     public class FilmeController : ControllerBase
@@ -15,20 +18,37 @@ namespace FilmesAPI.Controllers
         private FilmeContext _context;
         private IMapper _mapper;
 
+        ///<summary>
+        ///API que disponibiliza métodos RestFufll para Filmes
+        ///</summary>
         public FilmeController(FilmeContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
-                  
-        [HttpGet]
-        public IEnumerable<ReadFilmeDto> ConsultaFilme([FromQuery] int skip = 0, [FromQuery] int take = 50)
+
+        ///<summary>
+        ///Consulta filmes no banco de dados
+        ///</summary>
+        ///<param name="skip">skip</param>
+        ///<param name="take">take</param>
+        ///<returns>IActionResult</returns>
+        ///<response code="200">Retorna uma listta de filme</response>                  
+        [HttpGet("Consultar")]
+        public IEnumerable<ReadFilmeDto> Consultar([FromQuery] int skip = 0, [FromQuery] int take = 50)
         {
             return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
         }
 
-        [HttpGet("{id}")]
-        public IActionResult ConsultaFilmePorId(int id)
+        ///<summary>
+        ///Consulta um filme através do seu Id no banco de dados
+        ///</summary>
+        ///<param name="id">Id do filme</param>
+        ///<returns>IActionResult</returns>
+        ///<response code="200">Retorna o objeto filme</response>
+        [HttpGet("ConsultarPorId/{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult ConsultarPorId(int id)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
@@ -48,29 +68,29 @@ namespace FilmesAPI.Controllers
         ///<param name="filmeDto">Objeto com os campos necessários para criação de um filme</param>
         ///<returns>IActionResult</returns>
         ///<response code="201">Caso a inserção seja realizada com sucesso</response>
-        [HttpPost]
+        [HttpPost("Adicionar")]
         [ProducesResponseType(StatusCodes.Status201Created)]
-        public IActionResult AdicionaFilme([FromBody] CreateFilmeDto filmeDto)
+        public IActionResult Adicionar([FromBody] CreateFilmeDto filmeDto)
         {
             Filme filme = _mapper.Map<Filme>(filmeDto);            
             _context.Filmes.Add(filme);
             _context.SaveChanges();
 
-            return CreatedAtAction(nameof(ConsultaFilmePorId), new { id = filme.Id }, filme);
+            return CreatedAtAction(nameof(ConsultarPorId), new { id = filme.Id }, filme);
         }
 
 
 
         ///<summary>
-        ///Atualiza o objeto completo no banco de dados
+        ///Atualiza um filme no banco de dados
         ///</summary>
         ///<param name="id">Id do filme</param>
         ///<param name="filmeDto">Objeto com os novos dados</param>
         ///<returns>IActionResult</returns>
         ///<response code="204">Caso a atualização seja realizada com sucesso</response>
-        [HttpPut("{id}")]
+        [HttpPut("Atualizar/{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
+        public IActionResult Atualizar(int id, [FromBody] UpdateFilmeDto filmeDto)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
@@ -85,8 +105,17 @@ namespace FilmesAPI.Controllers
             return NoContent();
         }
 
-        [HttpPatch("{id}")]
-        public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+
+        ///<summary>
+        ///Atualiza um filme no banco de dados
+        ///</summary>
+        ///<param name="id">Id do filme</param>
+        ///<param name="patch">Propriedades com os novos dados</param>
+        ///<returns>IActionResult</returns>
+        ///<response code="204">Caso a atualização seja realizada com sucesso</response>
+        [HttpPatch("Atualizar/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Atualizar(int id, JsonPatchDocument<UpdateFilmeDto> patch)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
@@ -109,8 +138,16 @@ namespace FilmesAPI.Controllers
             return NoContent();
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeletaFilme(int id)
+
+        ///<summary>
+        ///Deleta um filme do banco de dados
+        ///</summary>
+        ///<param name="id">Id para </param>
+        ///<returns>IActionResult</returns>
+        ///<response code="204">Caso a remoção seja realizada com sucesso</response>
+        [HttpDelete("Deletar/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Deletar(int id)
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
 
